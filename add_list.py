7f11406debecdf2a9d977f50054042745e9c67ca
Suvan Goel <suvan.goel.25@ucl.ac.uk>
2025-11-01 18:23:17 +0000

    Adds Job specifications to global list

---
+++ add_list.py
@@ -0,0 +1,6 @@
+job_spec_list = []
+
+def add_to_list(item: str):
+    """Takes job specification as string and adds it to the global list."""
+    job_spec_list.append(item)
+    return job_spec_list
